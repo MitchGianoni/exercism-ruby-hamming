41ae08f8bb2dabb8a4987bbe4a095054f758f123
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-24 20:56:24 -0400

    fixed failure where exception was not raised if two sequences were different lengths

--- hamming.rb
+++ hamming.rb
@@ -4,17 +4,20 @@ class Hamming
 	def self.compute(first, second)
 		first_array = first.split('')
 		second_array = second.split('')
-		counter = 0
+		counter = 0 
 		x = 0
-
-		while x < first_array.length
-			if first_array[x] != second_array[x]
-				counter += 1
+		
+		if first_array.length != second_array.length
+			raise ArgumentError.new("Sequences need to be equal length!")
+		else
+			while x < first_array.length
+				if first_array[x] != second_array[x]
+					counter += 1
+				end
+				x += 1
 			end
-			x += 1
+			return counter
 		end
-
-		return counter
 	end
 end
 
